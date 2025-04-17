30630ddcea81cf9059887cb2ab930e2b489647d9
Abhinav <gangurde.a@northeastern.edu>
2025-04-17 12:17:01 -0400

    Update the snowflake dataload

--- airflow/dags/services/snowflake/ticker_file.py
+++ airflow/dags/services/snowflake/ticker_file.py
@@ -1,39 +1,33 @@
 import base64
 import ftplib
+import requests
 import pandas as pd
 from io import StringIO, BytesIO
 
 def fetch_all_us_listed_companies(**kwargs):
-    ftp = ftplib.FTP('ftp.nasdaqtrader.com')
-    ftp.login()
-    ftp.encoding = 'utf-8'
-    ftp.cwd('SymbolDirectory')
+    urls = {
+        "nasdaqlisted": "https://www.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt",
+        "otherlisted": "https://www.nasdaqtrader.com/dynamic/SymDir/otherlisted.txt"
+    }
 
-    files = ["nasdaqlisted.txt", "otherlisted.txt"]
     dfs = []
+    for name, url in urls.items():
+        response = requests.get(url)
+        content = response.content.decode("utf-8")
+        df = pd.read_csv(StringIO(content), sep="|")[:-1]
 
-    for file in files:
-        lines = []
-        ftp.retrlines(f"RETR {file}", lines.append)
-        content = "\n".join(lines)
-        df = pd.read_csv(StringIO(content), sep="|")
-        df = df[:-1]  # remove footer row
-
-        if 'Symbol' in df.columns:
+        if "Symbol" in df.columns:
             df.rename(columns={'Symbol': 'ticker', 'Security Name': 'company_name'}, inplace=True)
-        elif 'ACT Symbol' in df.columns:
+        elif "ACT Symbol" in df.columns:
             df.rename(columns={'ACT Symbol': 'ticker', 'Security Name': 'company_name'}, inplace=True)
 
-        df = df[df['ticker'].notna() & (~df['ticker'].str.contains('\\$', na=False))]
+        df = df[df['ticker'].notna() & (~df['ticker'].str.contains(r'\$', na=False))]
         dfs.append(df[['ticker', 'company_name']])
 
-    ftp.quit()
-    combined_df = pd.concat(dfs, ignore_index=True).drop_duplicates()
+    final_df = pd.concat(dfs, ignore_index=True).drop_duplicates()
 
-    # Convert DataFrame to CSV BytesIO
     csv_buffer = BytesIO()
-    csv_bytes = csv_buffer.getvalue()
-    csv_base64 = base64.b64encode(csv_bytes).decode("utf-8")
-
-    # Push to XCom
+    final_df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
+    csv_base64 = base64.b64encode(csv_buffer.getvalue()).decode("utf-8")
     kwargs['ti'].xcom_push(key='ticker_csv_b64', value=csv_base64)
--- airflow/dags/snowflake_dataload_US_marketanalysis.py
+++ airflow/dags/snowflake_dataload_US_marketanalysis.py
@@ -32,7 +32,7 @@ def upload_tickers_to_s3(**kwargs):
 
 
 with DAG(
-    dag_id='custom_marketanalysis_raw_data_snowflake',
+    dag_id='custom_marketanalysis_raw_data_snowflake_v1',
     default_args=default_args,
     tags=['snowflake','dataload','market_analysis','freecompanydatset','marketplace'],
     description='Loads or reloads snowflakes tables dataset from Snowflake Marketplace for Freecompanydataset',
@@ -59,8 +59,53 @@ with DAG(
         provide_context=True
     )
 
-    # End task
-    end = EmptyOperator(task_id='end')
 
-    # DAG flow
-    start >> scraping_cmp_ticker_file >> upload_to_s3 >> end
+    with TaskGroup("snowflake_load") as snowflake_group:
+        # Snowflake task for table creation
+        create_snowflake_table = SnowflakeOperator(
+        task_id='create_snowflake_table',
+        snowflake_conn_id='snowflake_default',
+        sql="""
+            USE ROLE FIN_ROLE;
+            USE WAREHOUSE DBT_WH;
+
+            CREATE OR REPLACE TABLE TICKERS (
+                TICKER VARCHAR,
+                COMPANY_NAME VARCHAR);""")
+
+        snowflake_stage = SnowflakeOperator(
+        task_id='snowflake_stage',
+        snowflake_conn_id='snowflake_default',
+        sql="""
+            CREATE OR REPLACE STAGE ticker_s3_stage
+            URL = 's3://{{ var.value.AWS_BUCKET_NAME }}/'
+            CREDENTIALS = (
+                AWS_KEY_ID = '{{ var.value.AWS_ACCESS_KEY_ID }}',
+                AWS_SECRET_KEY = '{{ var.value.AWS_SECRET_ACCESS_KEY }}'
+            );""")
+
+        snowflake_format = SnowflakeOperator(
+        task_id='snowflake_format',
+        snowflake_conn_id='snowflake_default',
+        sql="""
+        CREATE OR REPLACE FILE FORMAT ticker_csv_format
+        TYPE = CSV
+        SKIP_HEADER = 1
+        FIELD_OPTIONALLY_ENCLOSED_BY='"'
+        NULL_IF = ('', 'NULL');""")
+
+        load_to_snowflake = SnowflakeOperator(
+            task_id='load_to_snowflake',
+            snowflake_conn_id='snowflake_default',
+            sql="""
+                COPY INTO TICKERS
+                FROM @ticker_s3_stage/freecompany_ticker/us_listed_tickers.csv
+                FILE_FORMAT = ticker_csv_format;""")
+
+        create_snowflake_table >> snowflake_stage >> snowflake_format >> load_to_snowflake
+
+    # End task
+    end = EmptyOperator(task_id='end')   
+
+    # DAG execution flow
+    start >> scraping_cmp_ticker_file >> upload_to_s3 >> snowflake_group >> end
